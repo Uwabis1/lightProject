7064cd018e2a1864c1d41e7fe803c2b41e0a3540
Uwabis <uwabis@yahoo.com>
2021-10-17 15:14:25 +0100

    third commit

---
+++ test/Inbox.test.js
@@ -0,0 +1,66 @@
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require("../compile");
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there!"],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
+});
+
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    console.log(inbox);
+  });
+});
+
+
+
+
+
+
+
+
+
+
+/*
+this just shows hos mocha functions; it, decribe, beforeEach works!
+class Car {
+    park() {
+      return "stopped";
+    }
+    drive() {
+      return "vroom";
+    }
+  }
+  
+  let car; 
+
+
+  beforeEach(()=>{
+    car = new Car();
+  }); 
+
+  describe('car', () => {
+      it('can park', () => {
+          //const car = new Car();
+          assert.equal(car.park(), 'stopped'); 
+
+      });
+
+      it('car can drive', () => {
+          //const car = new Car(); 
+          assert.equal(car.drive(), 'vroom');
+      });
+  });
+  */ 
